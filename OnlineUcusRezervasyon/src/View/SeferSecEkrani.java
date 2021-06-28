3b6b0678e15a13ec5432aab2514adda94eae4bba
Burak Karagöz <81164258+burakaragoz4@users.noreply.github.com>
2021-06-28 14:39:19 +0300

    Update SeferSecEkrani.java

--- OnlineUcusRezervasyon/src/View/SeferSecEkrani.java
+++ OnlineUcusRezervasyon/src/View/SeferSecEkrani.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package arayuzu;
+package View;
 
-import ayarlar.ButtonAyarlari;
+import Model.ButtonAyarlari;
 import java.awt.Color;
 
 /**
